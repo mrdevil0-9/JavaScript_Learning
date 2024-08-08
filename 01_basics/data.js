e8274575dacc10035d8f71839eb706e66530fd3d
mrdevil0-9 <85818994+mrdevil0-9@users.noreply.github.com>
2024-08-08 09:55:01 +0000

    data

---
+++ 01_basics/data.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all js code as newer version
+//alert(3 + 3) we are using node js not browser
+ 
+
+let name = "Badal"
+let age = 19
+let state ;
+
+//symbol to identify the unique 
+//object 
+
+console.log(typeof "Badal Sharma");
+
+//null is a object 
+
+//undefine is data type 
+
+
