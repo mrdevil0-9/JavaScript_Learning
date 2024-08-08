e2130777b508222c1c2beea56371002c923d194a
mrdevil0-9 <85818994+mrdevil0-9@users.noreply.github.com>
2024-08-08 11:02:34 +0000

    data type

---
+++ 01_basics/datatype.js
@@ -0,0 +1,38 @@
+//primitive 
+//7 type  : String ,Number , Boolean , null , undefined , symbol,BigInt
+
+
+const score = 100
+const scorevalue = 100.5
+let userEmail ;
+
+
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id=== anotherId);
+//both is different 
+
+
+//Reference (Non primitive )
+
+//Array , Objects, Functions 
+
+//Array
+const heros = ["shaktima","ram"]
+
+//object 
+
+let myObj = {
+    name:"Badal",
+    age:23,
+
+}
+
+
+//function using variable
+
+const myFunction = function(){
+    console.log("Hello function ")
+}
