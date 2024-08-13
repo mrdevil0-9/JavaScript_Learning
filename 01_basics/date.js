30cbe0334a52864d44cdd44d549247a583b6c156
mrdevil0-9 <85818994+mrdevil0-9@users.noreply.github.com>
2024-08-13 10:22:39 +0000

    time

--- 01_basics/date.js
+++ 01_basics/date.js
@@ -1,1 +1,42 @@
+///date
+
 let myDate = new Date
+
+// console.log(myDate.toString());
+// //Tue Aug 13 2024 09:33:23 GMT+0000 (Coordinated Universal Time)
+
+
+// console.log(myDate.toDateString());
+// //Tue Aug 13 2024
+
+// console.log(myDate.toISOString());
+// //2024-08-13T09:35:45.455Z
+
+// console.log(myDate.toJSON());
+// //2024-08-13T09:36:24.422Z
+
+// console.log(myDate.toLocaleDateString());
+// //8/13/2024
+
+// console.log(myDate.toLocaleString());
+// //8/13/2024, 9:38:23 AM
+
+// console.log(myDate.toLocaleTimeString());
+// //9:39:10 AM
+
+// console.log(typeof myDate);
+// //object
+
+// let myCreatedDate = new Date(2023, 9 ,24)
+
+let myCreatedDate = new Date(2023, 0 ,24,0,9)
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+
+
+
+
+
